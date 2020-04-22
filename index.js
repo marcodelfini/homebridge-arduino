a9ea3874c4190f24e6e8417155b25fe775c40de1
Marco Delfini <marco_it15@hotmail.com>
2020-04-22 15:27:23 +0200

    279

--- index.js
+++ index.js
@@ -330,12 +330,12 @@ arduino.prototype.getServices = function () {
 										})
 		
 		const characteristicStatusFault = functionService.getCharacteristic(Characteristic.StatusFault)
-											.on("get", this.getValveStatusFault.bind(this))
-											.setInterval(()=> {
-												// use 'getvalue' when the timer ends so it triggers the .on('get'...) event
-												characteristicStatusFault.getValue();
-												this.log("update characteristicStatusFault "+characteristicStatusFault.value);
-											}, (5*1000));;
+										.on("get", this.getValveStatusFault.bind(this));
+										setInterval(()=> {
+											// use 'getvalue' when the timer ends so it triggers the .on('get'...) event
+											characteristicStatusFault.getValue();
+											this.log("update characteristicStatusFault "+characteristicStatusFault.value);
+										}, (5*1000));;
 
 		if(this.optionalCharac1 == true){
 			functionService.getCharacteristic(Characteristic.SetDuration)
